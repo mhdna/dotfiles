d2598c0686f4b70398f939ff48d44ed42fb435d3
Mahdi N <slmanmoh@outlook.com>
2023-01-18 18:27:44 +0200

    overriden some more options

--- .local/share/user-overrides.js
+++ .local/share/user-overrides.js
@@ -1,5 +1,32 @@
 user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
+
 user_pref("browser.uidensity", 1);
+
 user_pref("general.smoothScroll", false);
+
 user_pref("extensions.pocket.enabled", false);
+
 user_pref("keyword.enabled", true);
+
+user_pref("signon.rememberSignons", false);
+
+// Disable the Twitter/R*ddit/Faceberg ads in the URL bar:
+user_pref("browser.urlbar.quicksuggest.enabled", false);
+user_pref("browser.urlbar.suggest.topsites", false); // [FF78+]
+
+// Do not prefil forms:
+user_pref("signon.prefillForms", false);
+
+// Do not autocomplete in the URL bar:
+user_pref("browser.urlbar.autoFill", false);
+
+// Allow access to http (i.e. not https) sites:
+user_pref("dom.security.https_only_mode", false);
+
+// Keep cookies until expiration or user deletion:
+user_pref("network.cookie.lifetimePolicy", 0);
+
+user_pref("dom.webnotifications.serviceworker.enabled", false);
+
+// Disable push notifications:
+user_pref("dom.push.enabled", false);
