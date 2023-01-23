92b7a6508582d1e1356742fa9b4dbc37eabb2ac2
Mahdi N <slmanmoh@outlook.com>
2023-01-23 22:20:20 +0200

    disable for certain filetypes

--- .config/nvim/lua/core/lualine.lua
+++ .config/nvim/lua/core/lualine.lua
@@ -4,10 +4,6 @@ require('lualine').setup {
     theme = 'powerline_dark',
     -- component_separators = { left = '', right = ''},
     -- section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
     ignore_focus = {},
     always_divide_middle = true,
     globalstatus = false,
@@ -33,8 +29,4 @@ require('lualine').setup {
     lualine_y = {},
     lualine_z = {}
   },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
 }
