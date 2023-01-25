c02b8f361431b0c79767a9d147459307701b84d0
Mahdi N <slmanmoh@outlook.com>
2023-01-26 00:54:34 +0200

    needless comment

--- .config/nvim/lua/core/colorscheme.lua
+++ .config/nvim/lua/core/colorscheme.lua
@@ -2,7 +2,6 @@ vim.opt.bg = 'dark'
 vim.cmd(('hi Visual  guifg=black gui=none'))
 vim.cmd(('hi Pmenu guifg=yellow guibg=none'))
 vim.cmd(('hi PmenuSel guifg=none'))
--- vim.cmd(('colorscheme NeoSolarized'))
 vim.cmd(('hi SignColumn guibg=none '))
 vim.cmd(('hi Normal ctermbg=NONE guibg=NONE '))
 vim.cmd(('hi NormalFloat  guibg=none'))
