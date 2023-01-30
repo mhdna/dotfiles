9b167b832d18428fdc7976b54748f21b62926076
Mahdi N <slmanmoh@outlook.com>
2023-01-30 18:47:40 +0200

    removed unnecessary options

--- .local/share/user-overrides.js
+++ .local/share/user-overrides.js
@@ -2,7 +2,6 @@ user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
 user_pref("browser.uidensity", 1);
 user_pref("general.smoothScroll", false);
 user_pref("extensions.pocket.enabled", false);
-user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
 user_pref("keyword.enabled", true);
 user_pref("signon.rememberSignons", false);
 // Disable the Twitter/R*ddit/Faceberg ads in the URL bar:
@@ -12,11 +11,6 @@ user_pref("browser.urlbar.suggest.topsites", false); // [FF78+]
 user_pref("signon.prefillForms", false);
 // Do not autocomplete in the URL bar:
 user_pref("browser.urlbar.autoFill", false);
-// Allow access to http (i.e. not https) sites:
-user_pref("dom.security.https_only_mode", false);
-// Keep cookies until expiration or user deletion:
-user_pref("network.cookie.lifetimePolicy", 0);
-user_pref("dom.webnotifications.serviceworker.enabled", false);
 // Disable push notifications:
 user_pref("dom.push.enabled", false);
 // Disable automatic hiding for fullscreen
