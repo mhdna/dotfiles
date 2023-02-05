6a960537175ac937a3afaf6d231a56c68176d4d9
Mahdi N <slmanmoh@outlook.com>
2023-02-05 21:21:37 +0200

    moving plugins configs into "after" folder

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,67 @@
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(_, bufnr)
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<M-K>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<leader>lwa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<leader>lwr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<leader>lwl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<leader>ld', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, bufopts)
+end
+
+-- add completion capability
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+local lspconfig = require('lspconfig')
+
+lspconfig.ccls.setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+lspconfig.tsserver.setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+
+lspconfig.pylsp.setup {
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = {
+                    ignore = { 'W391' },
+                    maxLineLength = 100
+                }
+            }
+        }
+    }
+}
+
+require'lspconfig'.gopls.setup{}
+
+lspconfig.sumneko_lua.setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+    diagnostics = {
+        globals = { 'vim' },
+    },
+}
+
+
+-- Disable inline error messages
+vim.diagnostic.config({ virtual_text = false })
