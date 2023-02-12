755883b4b40e6130d9a56aa8a1b9605ef228fbd4
Mahdi N <slmanmoh@outlook.com>
2023-02-12 21:50:23 +0200

    some settings doesn't work unless they're at the top

--- .config/firefox/overrides.js
+++ .config/firefox/overrides.js
@@ -1,10 +1,16 @@
+// Disable Firefox sync and its menu entries
+user_pref("identity.fxaccounts.enabled", false);
+// Disable automatic hiding for fullscreen
+user_pref("browser.fullscreen.autohide", false);
+// do not show bookmarks in toolbar
+user_pref("browser.toolbars.bookmarks.visibility", "never")
+// disable firefox-view
+user_pref("browser.tabs.firefox-view", false)
 user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
 user_pref("browser.uidensity", 1);
-// user_pref("general.smoothScroll", false);
+user_pref("general.smoothScroll", false);
 user_pref("extensions.pocket.enabled", false);
-// user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
 user_pref("keyword.enabled", true);
-user_pref("signon.rememberSignons", false);
 // Disable the Twitter/R*ddit/Faceberg ads in the URL bar:
 user_pref("browser.urlbar.quicksuggest.enabled", false);
 user_pref("browser.urlbar.suggest.topsites", false); // [FF78+]
@@ -19,11 +25,3 @@ user_pref("network.cookie.lifetimePolicy", 0);
 user_pref("dom.webnotifications.serviceworker.enabled", false);
 // Disable push notifications:
 user_pref("dom.push.enabled", false);
-// Disable automatic hiding for fullscreen
-user_pref("browser.fullscreen.autohide", false);
-// do not show bookmarks in toolbar
-user_pref("browser.toolbars.bookmarks.visibility", "never")
-// disable firefox-view
-user_pref("browser.tabs.firefox-view", false)
-// Disable Firefox sync and its menu entries
-user_pref("identity.fxaccounts.enabled", false);
