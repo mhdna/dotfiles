6e404bdda9b3b30c40091ceaaf3e4cf2f078f7a5
Mahdi N <slmanmoh@outlook.com>
2023-02-15 20:35:28 +0200

    fixed contact template

--- .config/emacs/lisp/org-settings.el
+++ .config/emacs/lisp/org-settings.el
@@ -9,204 +9,210 @@
 (setq coding-system-for-read 'utf-8 ) ; use utf-8 by default
 (setq coding-system-for-write 'utf-8 )
 (use-package org
-  :config
-  (require 'org-tempo))
+	:config
+	(require 'org-tempo))
 ;; (setq initial-major-mode 'org-mode)
 ;; (setq org-hide-emphasis-markers t)
 ;; (use-package org-appear
 ;;   :hook (org-mode . org-appear-mode))
 (setq org-pretty-entities t)
 (setq org-startup-with-inline-images t
-      org-image-actual-width '(600))
+			org-image-actual-width '(600))
 (setq org-src-fontify-natively t)
 (setq org-src-tab-acts-natively t)
 (add-to-list 'org-structure-template-alist
-             '("el" . "src emacs-lisp"))
+						 '("el" . "src emacs-lisp"))
 (setq org-adapt-indentation nil)
 (setq org-directory "~/personal/notes/")
 (defun org-file-path (filename)
-  "Return the absolute address of an org file, given its relative name."
-  (concat (file-name-as-directory org-directory) filename))
+	"Return the absolute address of an org file, given its relative name."
+	(concat (file-name-as-directory org-directory) filename))
 (setq org-index-file (org-file-path "index.org"))
 (setq org-archive-location
-      (concat
-       (org-file-path (format "archive/archive-%s.org" (format-time-string "%Y")))
-       "::* From %s"))
+			(concat
+			 (org-file-path (format "archive/archive-%s.org" (format-time-string "%Y")))
+			 "::* From %s"))
 (setq org-refile-targets `((,org-index-file :level . 1)
-                           ;; (,(org-file-path "environment.org") :level . 1)
-                           (,(org-file-path "index.org") :level . 1)
-                           (,(org-file-path "goals.org") :level . 1)
-                           (,(org-file-path "links.org") :level . 1)
-                           (,(org-file-path "media.org") :level . 1)))
+													 ;; (,(org-file-path "environment.org") :level . 1)
+													 (,(org-file-path "index.org") :level . 1)
+													 (,(org-file-path "goals.org") :level . 1)
+													 (,(org-file-path "links.org") :level . 1)
+													 (,(org-file-path "media.org") :level . 1)))
 ;; (,(org-file-path "someday-maybe.org") :level . 1)
 ;; (,(org-file-path "work.org") :level . 1)))
 (setq org-agenda-files (list org-index-file
-                             ;; (org-file-path "calendars")
-                             (org-file-path "goals.org")
-                             ;; (org-file-path "tasks.org")
-                             (org-file-path "habits.org")
-                             (org-file-path "news.org")
-                             ;; (org-file-path "recurring-tasks.org")
-                             ;; (org-file-path "work.org")
-                             ))
+														 ;; (org-file-path "calendars")
+														 (org-file-path "goals.org")
+														 ;; (org-file-path "tasks.org")
+														 (org-file-path "habits.org")
+														 (org-file-path "news.org")
+														 ;; (org-file-path "recurring-tasks.org")
+														 ;; (org-file-path "work.org")
+														 ))
 (defun my/mark-done-and-archive ()
-  "Mark the state of an org-mode item as DONE, archive it, and
+	"Mark the state of an org-mode item as DONE, archive it, and
 save the Org buffers."
-  (interactive)
-  (org-todo 'done)
-  (org-archive-subtree)
-  (org-save-all-org-buffers))
+	(interactive)
+	(org-todo 'done)
+	(org-archive-subtree)
+	(org-save-all-org-buffers))
 (setq org-log-done 'time)
 ;; (setq org-log-done 'note)
 (setq org-enforce-todo-dependencies t)
 (defun my/org-add-tag (new-tag)
-  (org-set-tags (cons new-tag
-                      (seq-remove (lambda (tag)
-                                    (get-text-property 0 'inherited tag))
-                                  (org-get-tags)))))
+	(org-set-tags (cons new-tag
+											(seq-remove (lambda (tag)
+																		(get-text-property 0 'inherited tag))
+																	(org-get-tags)))))
 (defun my/schedule-today ()
-  "Tag this item with `daily'."
-  (interactive)
-  (my/org-add-tag "daily")
-  (save-buffer))
+	"Tag this item with `daily'."
+	(interactive)
+	(my/org-add-tag "daily")
+	(save-buffer))
 (setq org-agenda-start-on-weekday nil)
 ;; (setq org-deadline-warning-days 0)
 (setq org-agenda-prefix-format '((agenda . " %i %?-12t% s")
-                                 (todo . " %i ")
-                                 (tags . " %i ")
-                                 (search . " %i ")))
+																 (todo . " %i ")
+																 (tags . " %i ")
+																 (search . " %i ")))
 ;; (setq org-agenda-dim-blocked-tasks 'invisible)
 (defun org-agenda-delete-empty-blocks ()
-  "Remove empty agenda blocks.
+	"Remove empty agenda blocks.
 A block is identified as empty if there are fewer than 2
 non-empty lines in the block (excluding the line with
 `org-agenda-block-separator' characters)."
-  (when org-agenda-compact-blocks
-    (user-error "Cannot delete empty compact blocks"))
-  (setq buffer-read-only nil)
-  (save-excursion
-    (goto-char (point-min))
-    (let* ((blank-line-re "^\\s-*$")
-           (content-line-count (if (looking-at-p blank-line-re) 0 1))
-           (start-pos (point))
-           (block-re (format "%c\\{10,\\}" org-agenda-block-separator)))
-      (while (and (not (eobp)) (forward-line))
-        (cond
-         ((looking-at-p block-re)
-          (when (< content-line-count 2)
-            (delete-region start-pos (1+ (point-at-bol))))
-          (setq start-pos (point))
-          (forward-line)
-          (setq content-line-count (if (looking-at-p blank-line-re) 0 1)))
-         ((not (looking-at-p blank-line-re))
-          (setq content-line-count (1+ content-line-count)))))
-      (when (< content-line-count 2)
-        (delete-region start-pos (point-max)))
-      (goto-char (point-min))
-      ;; The above strategy can leave a separator line at the beginning
-      ;; of the buffer.
-      (when (looking-at-p block-re)
-        (delete-region (point) (1+ (point-at-eol))))))
-  (setq buffer-read-only t))
+	(when org-agenda-compact-blocks
+		(user-error "Cannot delete empty compact blocks"))
+	(setq buffer-read-only nil)
+	(save-excursion
+		(goto-char (point-min))
+		(let* ((blank-line-re "^\\s-*$")
+					 (content-line-count (if (looking-at-p blank-line-re) 0 1))
+					 (start-pos (point))
+					 (block-re (format "%c\\{10,\\}" org-agenda-block-separator)))
+			(while (and (not (eobp)) (forward-line))
+				(cond
+				 ((looking-at-p block-re)
+					(when (< content-line-count 2)
+						(delete-region start-pos (1+ (point-at-bol))))
+					(setq start-pos (point))
+					(forward-line)
+					(setq content-line-count (if (looking-at-p blank-line-re) 0 1)))
+				 ((not (looking-at-p blank-line-re))
+					(setq content-line-count (1+ content-line-count)))))
+			(when (< content-line-count 2)
+				(delete-region start-pos (point-max)))
+			(goto-char (point-min))
+			;; The above strategy can leave a separator line at the beginning
+			;; of the buffer.
+			(when (looking-at-p block-re)
+				(delete-region (point) (1+ (point-at-eol))))))
+	(setq buffer-read-only t))
 (add-hook 'org-agenda-finalize-hook #'org-agenda-delete-empty-blocks)
 (setq org-agenda-custom-commands '())
 (add-to-list 'org-agenda-custom-commands
-             '("p" "Personal agenda"
-               ((tags-todo "plan"
-                           ((org-agenda-overriding-header "Index")
-                            (org-agenda-hide-tags-regexp "plan\\|daily\\|weekly\\|yearly")))
-                (tags-todo "daily"
-                           ((org-agenda-overriding-header "Today")
-                            (org-agenda-hide-tags-regexp "habit\\|daily\\|weekly\\|yearly")))
-                (tags-todo "habit-daily-weekly"
-                           ((org-agenda-overriding-header "Habits")
-                            (org-agenda-hide-tags-regexp "habit")))
-                (tags-todo "weekly-daily"
-                           ((org-agenda-overriding-header "This Week")
-                            (org-agenda-hide-tags-regexp "weekly\\|yearly")))
-                (agenda ""
-                        (
-                         ;; (org-agenda-overriding-header "Calendar")
-                         (org-agenda-tag-filter-preset '("-daily"
-                                                         "-habit"
-                                                         "-weekly"
-                                                         "-yearly"))
-                         (org-agenda-hide-tags-regexp "daily\\|weekly\\|yearly")))
-                (tags-todo "yearly-daily-weekly"
-                           ((org-agenda-overriding-header "Annual Goals")
-                            (org-agenda-hide-tags-regexp "yearly"))))
-               ((org-agenda-skip-deadline-if-done t)
-                (org-agenda-skip-scheduled-if-done t)
-                (org-agenda-skip-timestamp-if-done t)
-                (org-agenda-tag-filter-preset '("-duplicate" "-news" "-writing")))))
+						 '("p" "Personal agenda"
+							 ((tags-todo "plan"
+													 ((org-agenda-overriding-header "Index")
+														(org-agenda-hide-tags-regexp "plan\\|daily\\|weekly\\|yearly")))
+								(tags-todo "daily"
+													 ((org-agenda-overriding-header "Today")
+														(org-agenda-hide-tags-regexp "habit\\|daily\\|weekly\\|yearly")))
+								(tags-todo "habit-daily-weekly"
+													 ((org-agenda-overriding-header "Habits")
+														(org-agenda-hide-tags-regexp "habit")))
+								(tags-todo "weekly-daily"
+													 ((org-agenda-overriding-header "This Week")
+														(org-agenda-hide-tags-regexp "weekly\\|yearly")))
+								(agenda ""
+												(
+												 ;; (org-agenda-overriding-header "Calendar")
+												 (org-agenda-tag-filter-preset '("-daily"
+																												 "-habit"
+																												 "-weekly"
+																												 "-yearly"))
+												 (org-agenda-hide-tags-regexp "daily\\|weekly\\|yearly")))
+								(tags-todo "yearly-daily-weekly"
+													 ((org-agenda-overriding-header "Annual Goals")
+														(org-agenda-hide-tags-regexp "yearly"))))
+							 ((org-agenda-skip-deadline-if-done t)
+								(org-agenda-skip-scheduled-if-done t)
+								(org-agenda-skip-timestamp-if-done t)
+								(org-agenda-tag-filter-preset '("-duplicate" "-news" "-writing")))))
 (defun my/org-agenda-delete-empty-blocks ()
-  "Remove empty agenda blocks.
+	"Remove empty agenda blocks.
 A block is identified as empty if there are fewer than 2
 non-empty lines in the block (excluding the line with
 `org-agenda-block-separator' characters)."
-  (when org-agenda-compact-blocks
-    (user-error "Cannot delete empty compact blocks"))
-  (setq buffer-read-only nil)
-  (save-excursion
-    (goto-char (point-min))
-    (let* ((blank-line-re "^\\s-*$")
-           (content-line-count (if (looking-at-p blank-line-re) 0 1))
-           (start-pos (point))
-           (block-re (format "%c\\{10,\\}" org-agenda-block-separator)))
-      (while (and (not (eobp)) (forward-line))
-        (cond
-         ((looking-at-p block-re)
-          (when (< content-line-count 2)
-            (delete-region start-pos (1+ (point-at-bol))))
-          (setq start-pos (point))
-          (forward-line)
-          (setq content-line-count (if (looking-at-p blank-line-re) 0 1)))
-         ((not (looking-at-p blank-line-re))
-          (setq content-line-count (1+ content-line-count)))))
-      (when (< content-line-count 2)
-        (delete-region start-pos (point-max)))
-      (goto-char (point-min))
-      ;; The above strategy can leave a separator line at the beginning
-      ;; of the buffer.
-      (when (looking-at-p block-re)
-        (delete-region (point) (1+ (point-at-eol))))))
-  (setq buffer-read-only t))
+	(when org-agenda-compact-blocks
+		(user-error "Cannot delete empty compact blocks"))
+	(setq buffer-read-only nil)
+	(save-excursion
+		(goto-char (point-min))
+		(let* ((blank-line-re "^\\s-*$")
+					 (content-line-count (if (looking-at-p blank-line-re) 0 1))
+					 (start-pos (point))
+					 (block-re (format "%c\\{10,\\}" org-agenda-block-separator)))
+			(while (and (not (eobp)) (forward-line))
+				(cond
+				 ((looking-at-p block-re)
+					(when (< content-line-count 2)
+						(delete-region start-pos (1+ (point-at-bol))))
+					(setq start-pos (point))
+					(forward-line)
+					(setq content-line-count (if (looking-at-p blank-line-re) 0 1)))
+				 ((not (looking-at-p blank-line-re))
+					(setq content-line-count (1+ content-line-count)))))
+			(when (< content-line-count 2)
+				(delete-region start-pos (point-max)))
+			(goto-char (point-min))
+			;; The above strategy can leave a separator line at the beginning
+			;; of the buffer.
+			(when (looking-at-p block-re)
+				(delete-region (point) (1+ (point-at-eol))))))
+	(setq buffer-read-only t))
 (add-hook 'org-agenda-finalize-hook #'my/org-agenda-delete-empty-blocks)
 (defun my/dashboard ()
-  (interactive)
-  (call-process-shell-command "daily-checklist")
-  (delete-other-windows)
-  (find-file org-index-file)
-  (org-agenda nil "p"))
+	(interactive)
+	(call-process-shell-command "daily-checklist")
+	(delete-other-windows)
+	(find-file org-index-file)
+	(org-agenda nil "p"))
 (defadvice org-agenda-set-mode-name (after truncate-org-agenda-mode-name activate)
-  (setq mode-name '("Org-agenda")))
+	(setq mode-name '("Org-agenda")))
 (add-to-list 'org-agenda-custom-commands
-             '("n" "News from this week"
-               ((agenda ""))
-               ((org-agenda-overriding-header "News from this week")
-                (org-agenda-start-day "-6d")
-                (org-agenda-span 14)
-                (org-agenda-files '(org-file-path "news.org"
-                                    ;; "recurring-events.org"
-                                    "books-read.org"
-                                    ;;                                         "papers-read.org"
-                                    )))))
+						 '("n" "News from this week"
+							 ((agenda ""))
+							 ((org-agenda-overriding-header "News from this week")
+								(org-agenda-start-day "-6d")
+								(org-agenda-span 14)
+								(org-agenda-files '(org-file-path "news.org"
+																									;; "recurring-events.org"
+																									"books-read.org"
+																									;;                                         "papers-read.org"
+																									)))))
 (add-to-list 'org-agenda-custom-commands
-             '("w" "Writing prompts"
-               ((tags "+writing"))
-               ((org-agenda-overriding-header "Writing prompts")
-                (org-agenda-sorting-strategy '((agenda ts-down))))))
+						 '("w" "Writing prompts"
+							 ((tags "+writing"))
+							 ((org-agenda-overriding-header "Writing prompts")
+								(org-agenda-sorting-strategy '((agenda ts-down))))))
 (defvar org-capture-templates '())
 (add-to-list 'org-capture-templates
-             '("p" "Project idea"
-               entry
-               (file "project-idea.org")
-               "* %?\n"))
+						 '("p" "Project idea"
+							 entry
+							 (file "project-idea.org")
+							 "* %?\n"))
 (add-to-list 'org-capture-templates
-             '("c" "Contact"
-               entry
-               (file "contacts.org")
-               "* %(org-contacts-template-name"))
+						 '("c" "Contact"
+							 entry
+							 (file "~/documents/contacts.org")
+							 "* %(org-contacts-template-name)
+:PROPERTIES:
+:ADDRESS: %^{123 Fake St., City, ST 12345}
+:PHONE: %^{555-555-5555}
+:EMAIL: %^{email}
+:NOTE: %^{note}
+:END:"))
 ;; (add-to-list 'org-capture-templates
 ;;              '("d" "Delivery"
 ;;                entry
