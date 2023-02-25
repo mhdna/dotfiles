685fb811ed0909b5377ee30474ee0a4af8130b2a
Mahdi N <slmanmoh@outlook.com>
2023-02-25 11:08:00 +0200

    no more additional colors

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,7 +1,6 @@
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    theme = 'powerline_dark',
     disabled_filetypes = {
         'help', 'qf', 'man', 'lspinfo', 'prompt'
     },
--- .config/nvim/lua/core/colorscheme.lua
+++ .config/nvim/lua/core/colorscheme.lua
@@ -1,10 +1,11 @@
 vim.opt.bg = 'dark'
-vim.cmd(('hi Visual  guifg=black gui=none'))
-vim.cmd(('hi Pmenu guifg=yellow guibg=none'))
-vim.cmd(('hi PmenuSel guifg=none'))
-vim.cmd(('hi SignColumn guibg=none '))
-vim.cmd(('hi Normal ctermbg=NONE guibg=NONE '))
-vim.cmd(('hi NormalFloat  guibg=none'))
+-- vim.cmd(('hi Visual  guifg=black gui=none'))
+-- vim.cmd(('hi CursorLine guibg=#222222'))
+-- vim.cmd(('hi Pmenu guifg=yellow guibg=none'))
+-- vim.cmd(('hi PmenuSel guifg=none'))
+-- vim.cmd(('hi SignColumn guibg=none '))
+-- vim.cmd(('hi Normal ctermbg=NONE guibg=NONE '))
+-- vim.cmd(('hi NormalFloat  guibg=none'))
 
 -- light
 -- vim.opt.bg = 'light'
