25781fefd7a733e63462b51af06393ed5df99b2b
Mahdi N <slmanmoh@outlook.com>
2023-02-26 11:45:35 +0200

    automatically insert snippet when pressing tab

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -16,17 +16,17 @@ local check_backspace = function()
 end
 
 -- Only Enable for certain filetypes
-cmp.setup.filetype({ 'c', 'cpp', 'java', 'python', 'javascript', 'go'}, {
+cmp.setup.filetype({ 'c', 'cpp', 'java', 'python', 'javascript', 'go' }, {
     enabled = function()
-      -- disable completion in comments
-      local context = require 'cmp.config.context'
-      -- keep command mode completion enabled when cursor is in a comment
-      if vim.api.nvim_get_mode().mode == 'c' then
-        return true
-      else
-        return not context.in_treesitter_capture("comment")
-          and not context.in_syntax_group("Comment")
-      end
+        -- disable completion in comments
+        local context = require 'cmp.config.context'
+        -- keep command mode completion enabled when cursor is in a comment
+        if vim.api.nvim_get_mode().mode == 'c' then
+            return true
+        else
+            return not context.in_treesitter_capture("comment")
+                and not context.in_syntax_group("Comment")
+        end
     end,
 })
 
@@ -44,7 +44,7 @@ cmp.setup {
     mapping = {
         ["<C-p>"] = cmp.mapping.select_prev_item(),
         ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs( -1), { "i", "c" }),
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
         -- ["<Tab>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
         ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
@@ -56,12 +56,12 @@ cmp.setup {
         -- Set `select` to `false` to only confirm explicitly selected items.
         ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expandable() then
+            if luasnip.expandable() then
                 luasnip.expand()
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
+            elseif cmp.visible() then
+                cmp.select_next_item()
             elseif check_backspace() then
                 cmp.mapping.complete()
                 fallback()
@@ -69,37 +69,37 @@ cmp.setup {
                 fallback()
             end
         end, {
-        "i",
-        "s",
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-            cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-        else
-            fallback()
-        end
-    end, {
-    "i",
-    "s",
-}),
+            "i",
+            "s",
+        }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable( -1) then
+                luasnip.jump( -1)
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
     },
     formatting = {
-        fields = {"abbr", "menu" },
+        fields = { "abbr", "menu" },
         format = function(entry, vim_item)
             vim_item.menu = ({
-                nvim_lsp = "[LSP]",
-                luasnip = "[Snippet]",
-                buffer = "[Buffer]",
-                path = "[Path]",
-            })[entry.source.name]
+                    luasnip = "[Snippet]",
+                    nvim_lsp = "[LSP]",
+                    buffer = "[Buffer]",
+                    path = "[Path]",
+                })[entry.source.name]
             return vim_item
         end,
     },
     sources = {
+        { name = 'luasnip' },
         { name = "nvim_lsp" },
-        { name = "luasnip" },
         { name = "path" },
         { name = "buffer" },
     },
