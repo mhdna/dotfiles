c7ec5c9e43cbb336d3c0178a28a43ba029cb80a7
Mahdi N <slmanmoh@outlook.com>
2023-03-02 20:55:52 +0200

    removed a needless parameter

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -25,8 +25,7 @@ local on_attach = function(_, bufnr)
 end
 
 -- add completion capability
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local lspconfig = require('lspconfig')
 
