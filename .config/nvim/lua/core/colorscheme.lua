414df97c3c9b1a2cf6be6e1241986799ac72ac7c
Mahdi N <slmanmoh@outlook.com>
2023-03-05 11:12:42 +0200

    removing needless colors

--- .config/nvim/lua/core/colorscheme.lua
+++ .config/nvim/lua/core/colorscheme.lua
@@ -1,6 +1,1 @@
 vim.opt.bg = 'dark'
-vim.cmd(('hi Visual  guifg=black gui=none'))
-vim.cmd(('hi Pmenu guifg=yellow guibg=none'))
-vim.cmd(('hi PmenuSel guifg=none'))
-vim.cmd(('hi Normal ctermbg=NONE guibg=NONE '))
-vim.cmd(('hi NormalFloat  guibg=none'))
