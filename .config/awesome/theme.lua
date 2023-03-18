bd6fbaaee548e2db2a7fed74ce2b86de206ba199
Mahdi N <slmanmoh@outlook.com>
2023-03-18 15:44:05 +0200

    default wallpaper

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -97,8 +97,8 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
--- theme.wallpaper = themes_path.."default/background.png"
-theme.wallpaper = os.getenv("HOME") .."/.local/share/bg"
+theme.wallpaper = themes_path.."default/background.png"
+-- theme.wallpaper = os.getenv("HOME") .."/.local/share/bg"
 
 -- theme.wibar_height = dpi (17)
 
