5c564f3127cf3b1b356bdb1704777ef4a60cd58b
Mahdi N <slmanmoh@outlook.com>
2023-03-26 22:45:43 +0300

    silent keybinds

--- .config/nvim/ftplugin/sh.lua
+++ .config/nvim/ftplugin/sh.lua
@@ -1,9 +1,9 @@
 vim.opt_local.makeprg="shellcheck -f gcc '%'"
 
-local opts = { noremap = true, buffer = 0 }
+local opts = { noremap = true, silent=true, buffer = 0 }
 
 vim.keymap.set('n', '<leader>c', ':w<CR>:silent make<CR>:cwindow<CR><C-w><C-k>', opts)
 vim.keymap.set('n', '<leader>q', ':cwindow<CR>', opts)
-vim.keymap.set('n', '<leader>S', ':source ~/.config/nvim/ftplugin/sh.lua<CR>', opts)
+vim.keymap.set('n', '<leader>s', ':source ~/.config/nvim/ftplugin/sh.lua<CR>', opts)
 vim.keymap.set('n', '<M-n>', ':cnext<CR>', opts)
 vim.keymap.set('n', '<M-p>', ':cprevious<CR>', opts)
