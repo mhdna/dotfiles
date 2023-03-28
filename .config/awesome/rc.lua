56db26a0f4959451ea1adbbff62018a3b6cda9be
Mahdi N <slmanmoh@outlook.com>
2023-03-28 19:21:05 +0200

    added low battery warning

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -793,4 +793,35 @@ client.connect_signal("request::geometry", function(c)
     end
  end)
 
+ -- battery warning
+-- created by bpdp
+
+local function trim(s)
+  return s:find'^%s*$' and '' or s:match'^%s*(.*%S)'
+end
+
+local function bat_notification()
+
+  local f_capacity = assert(io.open("/sys/class/power_supply/BAT0/capacity", "r"))
+  local f_status = assert(io.open("/sys/class/power_supply/BAT0/status", "r"))
+
+  local bat_capacity = tonumber(f_capacity:read("*all"))
+  local bat_status = trim(f_status:read("*all"))
+
+  if (bat_capacity <= 10 and bat_status == "Discharging") then
+    naughty.notify({ title      = "Battery Warning"
+      , text       = "Battery low! " .. bat_capacity .."%" .. " left!"
+      , fg="#ffffff"
+      , bg="#ff0000"
+      , timeout    = 15
+    })
+  end
+end
+
+battimer = timer({timeout = 120})
+battimer:connect_signal("timeout", bat_notification)
+battimer:start()
+
+-- end here for battery warning
+
 awful.spawn.with_shell("~/.config/awesome/autorun.sh")
