521b187b9f95b893c21a1a39d20624f9cda30a8c
Mahdi N <slmanmoh@outlook.com>
2023-03-30 14:15:10 +0200

    Enable all tags

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -191,7 +191,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1","2"}, --"3","4","5","6","7","8","9"},
+    awful.tag({ "1","2", "3","4","5","6","7","8","9"},
     s, {awful.layout.layouts[1]})
 
 
