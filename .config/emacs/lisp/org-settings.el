734a341d4765a7f570e6f63d043224c62e351b93
mhdna <mhdna@outlook.com>
2023-05-28 17:35:15 +0300

    better quotes capture template

--- .config/emacs/lisp/org-settings.el
+++ .config/emacs/lisp/org-settings.el
@@ -272,7 +272,7 @@ non-empty lines in the block (excluding the line with
 							("Q" "Quote"
 							 plain
 							 (file "quotes.org")
-							 "- %?\n")
+							 "** %?\t:%^{Tag(s)}:\n")
 							;;              ("d" "Delivery"
 							;;                entry
 							;;                (file+headline "deliveries.org" "Deliveries")
@@ -317,7 +317,7 @@ non-empty lines in the block (excluding the line with
 							("M" "Movie"
 							 plain
 							 (file "movies.org")
-							 "* %^{Title} -- %t   :%^{Tag}:\n** Stars: %^{Stars (out of 10)}/10\n** Degeneracy: %^{Degeneracy (out of 10)}/10\n** Review\n%?\n** Quotes\n")
+							 "* %^{Title} -- %t   :%^{Tag(s)}:\n** Stars: %^{Stars (out of 10)}/10\n** Degeneracy: %^{Degeneracy (out of 10)}/10\n** Review\n%?\n** Quotes\n")
 							("d" "Advice"
 							 plain
 							 (file "advice.org")
